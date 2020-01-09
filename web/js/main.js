b277b04a362bbf358ff8c78362273f0d0bf54bdf
liruigao <liruigao@cactus.local>
2020-01-09 16:39:23 +0800

    init article

--- web/js/main.js
+++ web/js/main.js
@@ -246,7 +246,8 @@
 			async: false,
 			dataType: "json",
 			success:function (data) {
-				loadBlogData(data.blogs);
+				loadData(data.blogs, "http://www.liruigao.top/cactus_blog/", "cactus-blogs");
+				loadData(data.articles, "http://www.liruigao.top/cactus_article/", "cactus-articles");
 			},
 			error: function (err) {
 				console.log("get summary json fail, check cactus_blog themes!", err);
@@ -254,31 +255,28 @@
 		})
 	}
 
-	function loadBlogData(blogs) {
-		var blog_url = "http://www.liruigao.top/cactus_blog/";
-		var blog_html = '';
-		for (var i = 0; i < blogs.length; i++) {
-			var blog_path = blog_url + blogs[i].path;
-			blog_html += '<div class="col-md-4">'
+	function loadData(content, content_url, content_id) {
+		var content_html = '';
+		for (var i = 0; i < content.length; i++) {
+			var content_path = content_url + content[i].path;
+			content_html += '<div class="col-md-4">'
 							+ '<div class="fh5co-blog animate-box">'
-								+ '<a href="' + blog_path + '" class="blog-bg" style="background-image: url(' + blogs[i].image + ');"></a>'
+								+ '<a href="' + content_path + '" class="blog-bg" style="background-image: url(' + content[i].image + ');"></a>'
 								+ '<div class="blog-text">'
-									+ '<span class="posted_on">' + blogs[i].date + '</span>'
-									+ '<h3><a href="' + blog_path + '">'+ blogs[i].title +'</a></h3>'
-									+ '<p>' + blogs[i].content + '</p>'
+									+ '<span class="posted_on">' + content[i].date + '</span>'
+									+ '<h3><a href="' + content_path + '">'+ content[i].title +'</a></h3>'
+									+ '<p>' + content[i].content + '</p>'
 									+ '<ul class="stuff">'
 										+ '<li><i class="icon-heart3"></i>249</li>'
 										+ '<li><i class="icon-eye2"></i>1,308</li>'
-										+ '<li><a href="' + blog_path + '">Read More<i class="icon-arrow-right22"></i></a></li>'
+										+ '<li><a href="' + content_path + '">Read More<i class="icon-arrow-right22"></i></a></li>'
 									+ '</ul>'
 								+ '</div>'
 							+ '</div>'
 						+ '</div>';
 		}
-		document.getElementById("cactus-blogs").innerHTML = blog_html;
+		document.getElementById(content_id).innerHTML = content_html;
 	}
-
-
 
 	$(function(){
 		initData();
